b657e41e9a6bb67a46cf4c220c43aec96016f2c8
Jenny (Hyun Ji) Seong <jennyseong77@gmail.com>
2017-04-13 15:20:33 -0400

    ✅Finished workshop

--- js/init.js
+++ js/init.js
@@ -3,5 +3,13 @@
 // parallax code from: http://materializecss.com/parallax.html
 // this initializes all modals so they can be triggered
 $(document).ready(function() {
-  //initializing code goes here!
+  // code from:http://materializecss.com/modals.html#!
+  // additional help from: http://stackoverflow.com/questions/41626343/materialized-modal-not-displaying
+  // this initializes all modals so they can be triggered
+  $('.modal').modal();
 });
+
+$(".dropdown-button").dropdown({ hover: false });
+
+// parallax code from: http://materializecss.com/parallax.html
+$('.parallax').parallax();
